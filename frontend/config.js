66597f169802cda9afafc996bef9a8dfa512e7b7
Delvin-Yamoah <delvin.yamoah@amalitechtraining.org>
2025-06-03 23:52:26 +0000

    Updated the config.js in the frontend directory and modified the gitignore to show the config.js file

---
+++ frontend/config.js
@@ -0,0 +1,6 @@
+const awsConfig = {
+  region: "eu-west-1",
+  userPoolId: "eu-west-1_JCjdxsabm",
+  userPoolWebClientId: "7hfa3dunnu08fjv5ihkf4eqlih",
+  apiEndpoint: "https://7ugmspx7g9.eu-west-1.awsapprunner.com/",
+};
