cf419b60575bca799165fded53309505e7c4cf11
Delvin-Yamoah <delvin.yamoah@amalitechtraining.org>
2025-06-04 12:40:22 +0000

    Modified the config.js file to use the right userpool credentials for cognito

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 // AWS resource configuration
 const awsConfig = {
   region: "eu-west-1",
-  userPoolId: "eu-west-1_DXi2cNzOu",
-  userPoolWebClientId: "138j7l7ro9jmf7ivpf3hg6tpvd",
+  userPoolId: "eu-west-1_JCjdxsabm",
+  userPoolWebClientId: "7hfa3dunnu08fjv5ihkf4eqlih",
   apiEndpoint: "https://7ugmspx7g9.eu-west-1.awsapprunner.com",
 };
