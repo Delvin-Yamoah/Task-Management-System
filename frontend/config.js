0f0bdb3ccc941a0f093d8a8d0511b10d61bf24c2
Delvin-Yamoah <delvin.yamoah@amalitechtraining.org>
2025-06-04 12:28:01 +0000

    Updated the config.js file to have the app runner endpoint

--- frontend/config.js
+++ frontend/config.js
@@ -3,5 +3,5 @@ const awsConfig = {
   region: "eu-west-1",
   userPoolId: "eu-west-1_DXi2cNzOu",
   userPoolWebClientId: "138j7l7ro9jmf7ivpf3hg6tpvd",
-  apiEndpoint: "https://main.dc15m8g1bzq89.amplifyapp.com/",
+  apiEndpoint: "https://7ugmspx7g9.eu-west-1.awsapprunner.com",
 };
